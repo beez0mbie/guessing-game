7b0ec6be61e382ad105d10c362140a025cdee5d6
Alexey Shmelkov <ashmelkov@aligntech.com>
2019-10-03 16:49:55 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,32 @@
 class GuessingGame {
-    constructor() {}
-
+    constructor(array, halfLength) {
+        this.array = array ;
+        this.halfLength = halfLength ;
+        }
     setRange(min, max) {
-
+        this.array = []
+        let arrayLength = max - min
+        for (let index = 0; index <= arrayLength; index++) {
+            this.array.push(min)
+            min++
+        }
+        return this.array
     }
-
+    
+    
     guess() {
-
+        this.halfLength = Math.floor(this.array.length / 2)
+        return this.array[this.halfLength]
     }
 
     lower() {
-
+        this.array = this.array.slice(0, this.halfLength + 1) 
+        return this.array 
     }
 
     greater() {
-
+        this.array = this.array.slice(this.halfLength)
+        return this.array
     }
 }
 
